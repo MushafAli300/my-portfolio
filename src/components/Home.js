9cc4495b15d6bd4c549a0590b5105bd2f767ad06
Mushaf Ali <alimushaf26@gmail.com>
2025-03-29 00:08:11 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1>Hi, I'm Mushaf Ali</h1>
+      <h1>Hi, I'm Mushaf Ali!</h1>
       <p>
         I'm an Undergraduate Software Engineer and Aspiring MERN Stack Developer with expertise in building high-performance web applications.  
         I love coding, problem-solving, and bringing creative ideas to life!  
